9a36e5a83e0a2ad0fdba780b82ec4d3a2e93f07e
rohanhacker <rohanvfx15@gmail.com>
2018-03-04 21:46:29 +0530

    register user and Get user info

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware("auth:api")->except('login','register');
+    }
+
     public function login()
     {
         if (Auth::attempt(['email' => request("email"), 'password' => request('password')])){
@@ -18,4 +26,36 @@ class AuthController extends Controller
             return response()->json(["error" => "Unauthorized", 401]);
         }
     }
+
+    public function register()
+    {
+        request()->all();
+        $validator = Validator::make(request()->all(),[
+           'name' => 'required',
+           'email' => 'required',
+           'password' => 'required'
+        ]);
+
+        if ($validator->fails()){
+            return response()->json(['errors' => $validator->errors()], 401);
+        }
+
+        $input = request()->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] = $user->createToken('myToken')->accessToken;
+        $success['name'] = $user->name;
+        $success['email'] = $user->email;
+
+        return response()->json(['success' => $success], 200);
+
+
+
+    }
+
+    public function UserInfo()
+    {
+        return response()->json(['user' => Auth::user()]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,13 @@ use Illuminate\Http\Request;
 */
 
 Route::post('login','AuthController@login');
+Route::post('register','AuthController@register');
 
+Route::get('user-info','AuthController@UserInfo');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+
 });
 
 Route::apiResource('products','ProductController');
